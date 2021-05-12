59febb97b04d509abcb0d36f6356c25f31569807
Dusan Odalovic <dodalovic@gmail.com>
2021-05-12 21:21:21 +0200

    Better tests

--- order-validator/src/test/kotlin/com/odalovic/simpleasyncdemo/OrderValidatorTest.kt
+++ order-validator/src/test/kotlin/com/odalovic/simpleasyncdemo/OrderValidatorTest.kt
@@ -8,20 +8,11 @@ internal class OrderValidatorTest {
 
     @Test
     fun `Validate should return list of valid items`() {
+
         val order = Order(
-            id = UUID.randomUUID(), items = listOf(
-                Order.OrderItem("foo-1", quantity = 1),
-                Order.OrderItem("foo-2", quantity = 2),
-                Order.OrderItem("foo-3", quantity = 3),
-                Order.OrderItem("foo-4", quantity = 4),
-                Order.OrderItem("foo-5", quantity = 5),
-                Order.OrderItem("foo-6", quantity = 6),
-                Order.OrderItem("foo-7", quantity = 7),
-                Order.OrderItem("foo-8", quantity = 8),
-                Order.OrderItem("foo-9", quantity = 9),
-                Order.OrderItem("foo-10", quantity = 10)
-            )
-        )
+            id = UUID.randomUUID(), items = (1..10).map {
+                Order.OrderItem("foo-$it", quantity = it)
+            })
         val (id, items) = OrderValidator().validate(order)
         id shouldBe order.id
         items.size shouldBe 4
@@ -30,15 +21,9 @@ internal class OrderValidatorTest {
     @Test
     fun `validate should return order with items as empty list`() {
         val order = Order(
-            id = UUID.randomUUID(), items = listOf(
-                Order.OrderItem("foo-5", quantity = 5),
-                Order.OrderItem("foo-6", quantity = 6),
-                Order.OrderItem("foo-7", quantity = 7),
-                Order.OrderItem("foo-8", quantity = 8),
-                Order.OrderItem("foo-9", quantity = 9),
-                Order.OrderItem("foo-10", quantity = 10)
-            )
-        )
+            id = UUID.randomUUID(), items = (5..10).map {
+                Order.OrderItem("foo-$it", quantity = it)
+            })
         val (id, items) = OrderValidator().validate(order)
         id shouldBe order.id
         items.size shouldBe 0
